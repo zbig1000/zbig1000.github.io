b0e544815179e535cd6d02b1b83a6468ac5cbda9
zbig1000 <zbig.lupina@gmail.com>
2018-03-06 21:19:27 +0100

    19.1 lesson - expanding view

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    const apiRoot = 'http://localhost:8080/v1/task/';
-    const trelloApiRoot = 'https://blooming-crag-57164.herokuapp.com/v1/task/';
+    const apiRoot = 'https://blooming-crag-57164.herokuapp.com/v1/task';
+    const trelloApiRoot = 'https://blooming-crag-57164.herokuapp.com/v1/trello/';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const $tasksContainer = $('[data-tasks-container]');
 
