5f19a81baf2ec7ec444d674a23319a30ab4d50c7
zbig1000 <zbig.lupina@gmail.com>
2018-03-08 19:50:03 +0100

    19.1 task heroku update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://blooming-crag-57164.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+  const trelloApiRoot = 'https://blooming-crag-57164.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
