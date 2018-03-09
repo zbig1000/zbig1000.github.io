bdd7840043bd22d258c5ceea9258b7d0beec17b0
unknown <zbig.lupina@gmail.com>
2018-03-09 18:55:26 +0100

    19.1 CORRECTED - heroku update

--- script.js
+++ script.js
@@ -104,11 +104,11 @@ $(document).ready(function() {
       }
     });
   }
-
+//    var requestUrl = apiRoot + 'deleteTask/' + taskId;
   function handleTaskDeleteRequest() {
     var parentEl = $(this).parents('[data-task-id]');
     var taskId = parentEl.attr('data-task-id');
-    var requestUrl = apiRoot + 'deleteTask/' + taskId;
+    var requestUrl = apiRoot + 'deleteTask/' + '15';
 
     $.ajax({
       url: requestUrl,
